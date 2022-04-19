98b3bb8fbe771646755abc721ead819059a50a11
Pablo Felipe <pablofelipe1999@hotmail.com>
2022-04-19 09:30:11 -0300

    Restructuring App component

--- src/App.js
+++ src/App.js
@@ -1,140 +1,125 @@
-import { useEffect } from "react"
-import weatherSVG from "./img/weather.svg"
+import { useEffect, useState } from "react"
 
 // Redux:
 import { useDispatch, useSelector } from "react-redux"
 import { fetchWeatherAction } from "./redux/slices/weather"
 
-//display icon https://openweathermap.org/img/wn/${icon}.png
-
 function App() {
+  const [city, setCity] = useState("")
+
   // Dispatch Action:
   const dispatch = useDispatch()
 
   useEffect(() => {
-    dispatch(fetchWeatherAction("curvelo"))
+    dispatch(fetchWeatherAction("birmingham"))
   }, [])
 
   // Select state from store:
   const state = useSelector((state) => state)
-  console.log(state)
-  const { weather, reducer, loading, error } = state
-  console.log(reducer)
+  const { weather, loading, error } = state.reducer
 
   return (
-    <div>
-      <section className="relative bg-gray-900  min-h-screen">
-        <img
-          className="w-56 lg:block lg:absolute top-0 left-0 pt-10"
-          src={weatherSVG}
-          alt="/"
-        />
-
-        <div className="relative container pt-12 px-4 mb-20 mx-auto text-center">
-          <span className="text-blue-500 font-semibold">
-            Built with react and redux
-          </span>
-          <h2 className="mt-8 mb-8 lg:mb-12 text-white text-4xl lg:text-6xl font-semibold">
-            Weather App
+    <div className="bg-gray-900">
+      <section className="relative bg-gray-900 min-h-screen">
+        <div className="relative container pt-6 px-4 mx-auto text-center">
+          <h2 className="mt-4 mb-4 lg:mb-12 text-white text-4xl lg:text-6xl font-semibold">
+            Weather <span className="text-green-600">App</span>
           </h2>
-          <p className="max-w-3xl mx-auto mb-8 lg:mb-12 text-white text-xl opacity-50">
-            Find out the current weather situation around the world
+          <p className="max-w-3xl mx-auto mb-4 lg:mb-10 text-white text-xl">
+            Find out the current weather in each city in the world!
           </p>
-          {/* Input */}
-          <input
-            placeholder="Search City"
-            className="relative z-10 inline-block w-full md:w-auto mb-2  px-3 py-2 mr-4  font-medium leading-normal bg-transparent border-2 rounded-lg text-green-400 "
-          ></input>
-          {/* Button */}
-          <button
-            type="button"
-            className="inline-flex items-center px-3 pr-3 28 text-center py-3 border border-transparent text-sm leading-4 font-medium rounded-md shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Search
-          </button>
+          <div className="sm:inline-flex sm:gap-4">
+            {/* Input */}
+            <input
+              value={city}
+              onChange={(event) => setCity(event.target.value)}
+              placeholder="Search City"
+              className="relative inline-block w-full md:w-auto px-3 py-3 font-medium leading-normal bg-transparent border-2 rounded-lg text-green-400"
+            ></input>
+            {/* Button */}
+            <button
+              onClick={() => dispatch(fetchWeatherAction(city))}
+              type="button"
+              className="inline-flex items-center px-6 text-center py-4 border border-transparent text-sm mt-4 sm:mt-0 leading-4 font-medium rounded-md shadow-sm text-gray-900 bg-green-600 hover:opacity-50 transition-all duration-300 focus:outline-none"
+            >
+              Search
+            </button>
+          </div>
         </div>
         {/* Content goes here */}
-        <div className="max-w-6xl px-4 mx-auto ">
-          <div className="flex flex-wrap -mx-4 justify-center">
-            <div className="w-full md:w-1/3 px-4">
-              <div className="p-8 border border-blue-800 rounded-lg">
-                <div className="flex justify-start  items-center">
-                  <span className="flex items-center justify-center w-16 h-16 rounded-full border-2">
-                    {/* weather logo */}
-                    <img
-                      className="w-56 "
-                      src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
-                      alt="/"
-                    />
-                  </span>
-                  <h1 className="text-gray-300 pl-5">
-                    {weather?.weather[0].main}
-                  </h1>{" "}
-                </div>
-                <h1 className="text-gray-300 text-center text-4xl mb-10">
-                  {/* {Math.ceil(Number(weather?.main.temp))}{" "} */}
-                  <span className="text-yellow-500 text-4xl">°C</span>
-                </h1>
-                <h3 className="mb-6 text-xl text-white font-semibold">
-                  {/* {weather?.name}, {weather?.sys?.country} */}
-                </h3>
-                <p className="mb-8 text-gray-300">
-                  {/* The weather condition in {weather?.name},{" "}
-                    {weather?.sys?.country} is described as :{" "}
-                    {weather?.weather[0].description} with a temperature of{" "}
-                    {Math.ceil(Number(weather?.main.temp))} °C and a humidity of{" "}
-                    {weather?.main?.humidity} % */}
-                </p>
-                <a
-                  className="ml-auto flex items-center justify-center w-20 h-20 rounded-full  hover:bg-blue-700 text-white"
-                  href="#"
-                >
-                  <span className="flex items-center justify-center w-16 h-16 rounded-full border-2">
-                    {/* weather logo */}
-                    {/* <img
-                        className="w-56 "
-                        src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}.png`}
+        {loading ? (<h1 className="text-gray-400 text-4xl text-center py-32">Loading, please wait...</h1>) : error ? (<h1 className="text-red-400 text-2xl text-center py-32">{error?.message.toUpperCase()}</h1>) : (
+          <div className="max-w-6xl px-4 mx-auto py-10">
+            <div className="flex flex-wrap -mx-4 justify-center">
+              <div className="w-full md:w-1/3 px-4">
+                <div className="p-8 border border-green-600 rounded-lg">
+                  <div className="flex justify-start  items-center">
+                    <span className="flex items-center justify-center w-16 h-16 rounded-full border-2">
+                      {/* weather logo */}
+                      <img
+                        className="w-56"
+                        src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
                         alt="/"
-                      /> */}
-                  </span>
-                </a>
+                      />
+                    </span>
+                    <h1 className="text-gray-300 pl-5">
+                      {weather?.weather[0].main}
+                    </h1>{" "}
+                  </div>
+                  <h1 className="text-gray-300 text-center text-4xl mb-10">
+                    {Math.ceil(Number(weather?.main.temp))}{" "}
+                    <span className="text-yellow-500 text-4xl">°C</span>
+                  </h1>
+                  <h3 className="mb-6 text-xl text-white font-semibold">
+                    {weather?.name}, {weather?.sys?.country}
+                  </h3>
+                  <p className="mb-8 text-gray-300 text-justify">
+                    The weather condition in {weather?.name},{" "}
+                    {weather?.sys?.country} is described as:{" "}
+                    <span className="font-semibold">
+                      {weather?.weather[0].description} with a temperature of{" "}
+                      {Math.ceil(Number(weather?.main.temp))} °C and a humidity of{" "}
+                      {weather?.main?.humidity}%.
+                    </span>
+                  </p>
+                  <a
+                    className="ml-auto flex items-center justify-center w-20 h-20 rounded-full  hover:bg-green-600 text-white"
+                    href="#"
+                  >
+                    <span className="flex items-center justify-center w-16 h-16 rounded-full border-2">
+                      {/* weather logo */}
+                      <img
+                        className="w-56"
+                        src={`https://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+                        alt="/"
+                      />
+                    </span>
+                  </a>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
+        {(!loading && !error?.message) &&
+          <div className="text-center">
+            <span className="text-white font-semibold">
+              Built with React and Redux
+            </span>
+          </div>
+        }
       </section>
       {/* Footer */}
-      <div className="text-center bg-red-900">
-        <p className="mb-4  text-gray-300">
-          Developed by
-          <span className="p-2 text-yellow-300">
-            <a href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw">
-              i-Novotek
+      <div className="text-center bg-gray-900 py-6">
+        <p className="text-white">
+          &copy; {new Date().getFullYear()} Weather App - Developed by{" "}
+          <span className="font-semibold">
+            <a className="hover:text-green-600 transition-all duration-300" href="https://github.com/PabloFLPs" target="_blank">
+              Pablo FELPs
             </a>
           </span>
         </p>
-        <a
-          className="inline-flex text-blue-400 hover:text-blue-500 font-bold"
-          href="https://www.youtube.com/channel/UCvu6J9q1AM6q4xysGqAvVyw"
-        >
-          <span className="mb-10">Watch the tutorial</span>
-          <svg
-            className="ml-4 w-4 h-5"
-            width="19"
-            height="20"
-            viewBox="0 0 19 20"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M18.7383 1.47342L18.7383 10.9304L17.5562 10.9304L17.5562 2.89788L0.834948 19.625L0.00154682 18.7916L16.7228 2.06448L9.28125 2.06448L9.28125 0.882355L18.1472 0.882355C18.4737 0.882355 18.7383 1.14697 18.7383 1.47342Z"
-              fill="#1F40FF"
-            ></path>
-          </svg>
-        </a>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
